df518b9644468e61d99e60c9c31655b3b962da71
Safu Harry <safuh8@gmail.com>
2024-10-11 23:34:19 +0300

    attaching database

--- src/functions/db_connect.ts
+++ src/functions/db_connect.ts
@@ -2,7 +2,7 @@ import pg from 'pg'
 const {Pool} =pg
 
 const pool=new Pool({
-	connectionString:'postgresql://yousir:iFWCBsnLO0AMrqDT6M8AOAGe2SijqY3t@dpg-cs4hlhjtq21c73fts8vg-a.oregon-postgres.render.com/selasql'
+	connectionString:'postgresql://yousir:iFWCBsnLO0AMrqDT6M8AOAGe2SijqY3t@dpg-cs4hlhjtq21c73fts8vg-a.oregon-postgres.render.com/selasql?sslmode=require'
 
 })
 export default pool
